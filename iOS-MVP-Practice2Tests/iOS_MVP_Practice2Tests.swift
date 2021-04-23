f78d151f059dfb20be7733c3cf17abaa53d3f963
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-04-23 11:23:15 +0900

    プロジェクトファイルを追加

---
+++ iOS-MVP-Practice2Tests/iOS_MVP_Practice2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_MVP_Practice2Tests.swift
+//  iOS-MVP-Practice2Tests
+//
+//  Created by 坂本龍哉 on 2021/04/23.
+//
+
+import XCTest
+@testable import iOS_MVP_Practice2
+
+class iOS_MVP_Practice2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
